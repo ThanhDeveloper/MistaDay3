c81b0659d6e608dd2348a017a8f5136df7589e50
ThanhDeveloper <thanhdev98@gmail.com>
2019-04-23 13:24:55 +0700

    Commit lab5 bonus find and update function

---
+++ JspDay3/src/lab5Ver2/Job.java
@@ -0,0 +1,11 @@
+package lab5Ver2;
+
+public class Job {
+	protected int idJob;
+	protected String nameJob;
+	public Job(int idJob, String nameJob) {
+		super();
+		this.idJob = idJob;
+		this.nameJob = nameJob;
+	}
+}
---
+++ JspDay3/src/lab5Ver2/ListManagement.java
@@ -0,0 +1,115 @@
+package lab5Ver2;
+
+import java.util.*;
+
+public class ListManagement {
+	public static int inputNumber;
+	public static Scanner input = new Scanner(System.in);
+	public static Employee[] empArr;
+	public static int empNo;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		showMenu();
+		System.out.println("The Program end !!! ");
+	}
+
+	private static void showMenu() {
+
+		do {
+			// menu CodeTodo
+
+			System.out.println("1: Create  new Employees");
+			System.out.println("2: Show the existing Employees");
+			System.out.println("3: find the existing Employees");
+			System.out.println("4: Update the existing Employees");
+			System.out.println("5: exit");
+			System.out.println("--------------------------------------");
+			System.out.println("Choose one number !");
+			inputNumber = input.nextInt();
+
+			switch (inputNumber) {
+			case 1: // add employ
+				addEmployees();
+				break;
+			case 2: // Show employ
+
+				showEmployees();
+				break;
+			case 3: // Find employ
+				FindEmployees();
+				break;
+			case 4: // Edit employ
+				UpdateEmployees();
+			case 5:
+				break;
+			default:
+				System.out.println(" Your input wrong,Enter again! ");
+				break;
+			}
+		} while (inputNumber != 5);
+
+	}
+
+	// UpdateEmp
+	private static void UpdateEmployees() {
+		System.out.println("Enter id employ to update ! ");
+		Integer upChoice = input.nextInt();
+		String name = input.next();
+		double salary = input.nextDouble();
+		Integer age = input.nextInt();
+		String jobName = input.next();
+		boolean Married = input.nextBoolean();
+		empArr[upChoice - 1].setName(name);
+		empArr[upChoice - 1].setSalary(salary);
+		empArr[upChoice - 1].setAge(age);
+		empArr[upChoice - 1].setJob(new Job(upChoice - 1, jobName));
+		empArr[upChoice - 1].setMarried(Married);
+
+	}
+
+	// FindEmp
+	private static void FindEmployees() {
+
+		System.out.println("Find with id or name");
+		Integer findChoice = input.nextInt();
+		// Check and find arrObject
+		// Check with id
+		if (findChoice > empArr.length) {
+			System.out.println("No id existed");
+		} else {
+			System.out.println(empArr[findChoice - 1]);
+		}
+		// check with name (RegexInput)
+
+	}
+
+	// ShowEmp
+	private static void showEmployees() {
+
+		for (int i = 0; i < empArr.length; i++) {
+			System.out.println(empArr[i].toString());
+		}
+	}
+
+	// AddEmp
+	private static void addEmployees() {
+
+		System.out.println("How many do you want to Create?");
+		empNo = input.nextInt();
+		empArr = new Employee[empNo]; // create size ObjectArray
+		// input data
+		for (int i = 0; i < empArr.length; i++) {
+			String name = input.next();
+			double salary = input.nextDouble();
+			Integer age = input.nextInt();
+			String jobName = input.next();
+			boolean Married = input.nextBoolean();
+			Employee empObj = new Employee(name, i + 1, salary, age, Married, new Job(i + 1, jobName));
+			empArr[i] = empObj;
+
+		}
+
+	}
+
+}
