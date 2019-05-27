49e1ccb6634aba5b2af01c56b3b220edd030c9fa
ThanhDeveloper <thanhdev98@gmail.com>
2019-05-27 23:28:06 +0700

    no message

--- JspDay6_ConnectDB/src/ConnectionUtils.java
+++ JspDay6_ConnectDB/src/ConnectionUtils.java
@@ -15,17 +15,27 @@ public class ConnectionUtils {
 		Connection conn=DriverManager.getConnection(connectionURL, username, password);
 		return conn;
 	}
-	public static void main(String[] args) {
+	//Load News table 
+	public static void loadNews() {
 		Connection conn=null;
 		try {
+			
 			conn=getConnection();
 			String sql="Select * from news";
 			Statement statement=conn.createStatement();
 			ResultSet rs=statement.executeQuery(sql);
+			System.out.println("|id | category_id | name         | description | detail |  image  |  date | user_id");
 			while (rs.next()) {
 				int id=  rs.getInt("id");
 				String name =rs.getString("name");
-				System.out.println("id : "+id+"|"+"name : "+name );
+				int category_id=  rs.getInt("category_id");
+				String description =rs.getString("description");
+				String detail =rs.getString("detail");
+				String image =rs.getString("image");
+				Date date=(Date) rs.getDate("Timestamp");
+				String user_id =rs.getString("user_id");
+				System.out.println("|"+id+"  | "+ category_id+"           | "+name+"          | "+description+" | "+detail+" |  "+image+"  | "+date+" | "+user_id );
+				
 				
 				
 			}
@@ -33,6 +43,44 @@ public class ConnectionUtils {
 			
 			e.printStackTrace();
 		}
+	}
+	//Load table news with FK Category
+	public static void loadCategory_Name() {
+		Connection conn=null;
+		try {
+			
+			conn=getConnection();
+			String sql="select * from categories \r\n" + 
+					"left join \r\n" + 
+					"news on news.category_id=categories.id";
+			Statement statement=conn.createStatement();
+			ResultSet rs=statement.executeQuery(sql);
+			System.out.println("|id | |name | |id | category_id | name         | description | detail |  image  |  date | user_id");
+			while (rs.next()) {
+				int id=  rs.getInt("id");
+				String name =rs.getString("name");
+				int category_id=  rs.getInt("category_id");
+				String description =rs.getString("description");
+				String detail =rs.getString("detail");
+				String image =rs.getString("image");
+				Date date=(Date) rs.getDate("Timestamp");
+				String user_id =rs.getString("user_id");
+				int idCate=  rs.getInt("id");
+				String nameCate=rs.getString("name");
+				System.out.println("|"+idCate+"|"+nameCate+"|"+id+"  | "+ category_id+"           | "+name+"          | "+description+" | "+detail+" |  "+image+"  | "+date+" | "+user_id );
+				
+				
+				
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		loadNews();
+		System.out.println();
+		loadCategory_Name();
 		
 	}
 }
