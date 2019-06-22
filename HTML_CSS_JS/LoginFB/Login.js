e9431254eb2ce3c6af75296c65187ca769c860d7
ThanhDeveloper <thanhdev98@gmail.com>
2019-06-22 22:13:02 +0700

    LoginFB.Responsive not yet

---
+++ HTML_CSS_JS/LoginFB/Login.js
@@ -0,0 +1,11 @@
+function CheckLogin(){
+	 var user=document.getElementById("username").value;
+	 var pass=document.getElementById("password").value;
+	
+	 if(user=="admin" && pass=="12345"){
+	 	alert('Login successfull! Welcome '+user);
+	 }
+	 else{
+	 	alert('Login failed!!!');
+	 }
+}
